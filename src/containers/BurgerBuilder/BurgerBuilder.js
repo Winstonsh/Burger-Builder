5689ecaefe848bcc10b25fa8119ae81f53c14b6c
Winstonsh <winston_halim@hotmail.com>
2020-08-21 09:25:54 +1000

    set up firebase database and added order axios post request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 
-import Burger from '../../components/Burger/Burger'
-import BuildControls from '../../components/Burger/BuildControls/BuildControls'
-import Modal from '../../components/UI/Modal/Modal'
-import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
+import axios from '../../axios-orders';
+
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -25,8 +27,25 @@ class BurgerBuilder extends Component {
         purchasing: false
     }
 
-    purchaseContinueHandler() {
-        alert('you continued');
+    purchaseContinueHandler = () => {
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            deliveryMethod: 'fastest',
+            customer: {
+                name: 'Winston Halim',
+                email: 'test@test.com',
+                address: {
+                    street: 'testStreet1',
+                    zipcode: 4152,
+                    country: 'Australia'
+                }
+            }
+        }
+
+        axios.post("/orders.json", order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error))
     }
 
     purchaseHandler = () => {
