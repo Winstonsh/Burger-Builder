292656d34761d8888b4ffcc16b6a698ce322e45f
Winstonsh <winston_halim@hotmail.com>
2020-09-04 17:57:07 +1000

    added Contact Data form and route

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,6 +1,9 @@
 import React, { Component} from 'react';
 
+import { Route } from 'react-router-dom';
+
 import CheckoutSummary from '../../components/CheckoutSummary/CheckoutSummary';
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
     state = {
@@ -37,6 +40,10 @@ class Checkout extends Component {
                     ingredients={this.state.ingredients}
                     checkoutCancelled={this.checkoutCancelled}
                     checkoutContinued={this.checkoutContinued}
+                />
+                <Route 
+                    path={this.props.match.path + "/contact-data"} 
+                    component={ContactData}
                 />
             </div>
         );
