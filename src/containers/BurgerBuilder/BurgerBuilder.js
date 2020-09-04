1b0776fa605ba7866d1e48271ab81bcec7b4ded6
Winstonsh <winston_halim@hotmail.com>
2020-09-04 16:57:06 +1000

    added query parameter to build burger in checkout page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -58,7 +58,17 @@ class BurgerBuilder extends Component {
         //     .catch(error => {
         //         this.setState({loading: false, purchasing: false})
         //     })
-        this.props.history.push('/checkout');
+        const queryParams = [];
+        for(let i in this.state.ingredients) {
+            //queryParams below is key value pair of ingredients (key=value)
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+        }
+
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString
+        });
     }
 
     purchaseHandler = () => {
