73ace2889ee86226c61a2390f6ecdd8896d48b3d
Winstonsh <winston_halim@hotmail.com>
2020-09-09 10:08:34 +1000

    output my orders dynamically from database

--- src/components/Burger/Order/Order.js
+++ src/components/Burger/Order/Order.js
@@ -2,11 +2,36 @@ import React from 'react';
 
 import classes from './Order.module.css';
 
-const order = () => {
+const order = (props) => {
+    const ingredients = [];
+
+    for(let ingredientName in props.ingredients) {
+        ingredients.push(
+            {
+                name: ingredientName,
+                amount: props.ingredients[ingredientName]
+            }
+        );
+    }
+
+    const ingredientsOutput = ingredients.map(ingredient => {
+        return(
+        <span
+        style={{
+            textTransform: 'capitalize',
+            display: 'inline-block',
+            margin: '0 8px',
+            border: '1px solid #ccc',
+            padding: '5px'
+        }}
+        >{ingredient.name} ({ingredient.amount})</span>
+        )
+    })
+
     return(
         <div className={classes.Order}>
-            <p>Ingredients: salad (1)</p>
-            <p>price: <strong>USD 5.45</strong></p>
+            <p>Ingredients: {ingredientsOutput}</p>
+            <p>price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong></p>
         </div>
     )
 }
