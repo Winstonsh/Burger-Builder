3d72e91e6cb87a420d50840f66830254458fd1ba
Winstonsh <winston_halim@hotmail.com>
2020-09-28 16:12:34 +1000

    adjust order now button with redux

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -13,7 +13,6 @@ import * as actionTypes from '../../store/actions';
 
 class BurgerBuilder extends Component {
     state = {
-        purchaseable: false,
         purchasing: false,
         loading: false,
         error: false
@@ -50,14 +49,13 @@ class BurgerBuilder extends Component {
         this.setState({purchasing: false})
     }
 
-    updatePurchaseState = () => {
-        const ingredients = {...this.state.ingredients};
+    updatePurchaseState = ( ingredients ) => {
         const sum = Object.keys(ingredients).map(igKey => {
             return ingredients[igKey];
         }).reduce((sum,el) => {
             return sum + el;
         },0)
-        this.setState({purchaseable: sum > 0})
+        return sum > 0
     }
 
 //REPLACED WITH REDUX
@@ -109,7 +107,7 @@ class BurgerBuilder extends Component {
                         ingredientRemoved={this.props.onIngredientRemoved}
                         disabledLessButton={disableLessButton}
                         price={this.props.price}
-                        purchaseable={this.state.purchaseable}
+                        purchaseable={this.updatePurchaseState(this.props.ings)}
                         ordered={this.purchaseHandler}
                     />
                 </React.Fragment>
